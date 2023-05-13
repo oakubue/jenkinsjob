6c02e07b6383aebf7015a6f09eabf9f43fa7fa91
Ubuntu <ubuntu@ip-172-31-11-186.us-east-2.compute.internal>
2023-05-13 20:18:23 +0000

    Upgrade from version 1.2 to 1.3

--- intro.sh
+++ intro.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "Hello, i hope you are having a great day"
+echo
+echo
+echo "Below are the list of the last ten users in this server"
+echo
+echo
+
+cat /etc/passwd | awk -F : "{print $1}" | tail -10
